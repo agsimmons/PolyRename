c82d0a0087884f974bda167f2db766da0b40531a
Andrew Simmons <agsimmons0@gmail.com>
2019-05-24 14:41:59 -0400

    Create testdriver for initial testing

---
+++ polyrename/testdriver.py
@@ -0,0 +1,52 @@
+import argparse
+from pathlib import Path
+
+from transformation import prefix, suffix
+
+TRANSFORMATION_CLASSES = [prefix.PrefixTransformation, suffix.SuffixTransformation]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='+')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    file_sequence = [Path(file) for file in args.files if Path(file).exists()]
+
+    print('Choose a transformation:')
+    for i, tranformation in enumerate(TRANSFORMATION_CLASSES):
+        print('{}) {}'.format(i, tranformation.schema['metadata']['name']))
+
+    # Ask for selection input
+    choice = None
+    while choice not in range(len(TRANSFORMATION_CLASSES)):
+        try:
+            choice = int(input('Selection: '))
+        except ValueError:
+            pass
+
+    # Gather arguments for selected transformation
+    transformation_class = TRANSFORMATION_CLASSES[choice]
+    transformation_args = []
+    for option in transformation_class.schema['options']:
+        input_prompt = '{} ({}):'.format(option['name'], option['description'])
+        choice = None
+        while not choice:
+            try:
+                choice = option['datatype'](input(input_prompt))
+            except ValueError:
+                print('Invalid input!')
+        transformation_args.append(choice)
+
+    # Create transformation with selected arguments
+    tranformation = transformation_class(file_sequence, *transformation_args)
+    transformed_file_sequence = tranformation.resolve()
+    for file_pairs in zip(file_sequence, transformed_file_sequence):
+        print('{} -> {}'.format(*file_pairs))
+
+
+if __name__ == '__main__':
+    main()
