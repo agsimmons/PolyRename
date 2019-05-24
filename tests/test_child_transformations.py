ecbc3cb4b96e490c053565bbc565926299f5bbba
Andrew Simmons <agsimmons0@gmail.com>
2019-05-24 14:53:01 -0400

    Update tests for new Tranformation API

--- tests/test_child_transformations.py
+++ tests/test_child_transformations.py
@@ -1,21 +1,18 @@
-from polyrename.transformation.child import string, sequence
+from pathlib import Path
+
+from polyrename.transformation import prefix, suffix
 
 
-def test_text():
-    """Verify that text transformation returns the same value it was initialized with"""
-    file_sequence = range(4)
-
-    my_string = 'Hello World!'
-    my_text = string.StringTransformation(file_sequence)
-    my_text.configure(my_string)
-    assert my_text.resolve() == my_string
+def test_prefix():
+    """Verify that prefix tranformation behaves as expected"""
+    file_sequence = [Path('/home/test/example.py'), Path('file_without_extension'), Path('relative.jpg')]
+    transformation = prefix.PrefixTransformation(file_sequence, 'HelloWorld')
+    assert transformation.resolve() == [Path('/home/test/HelloWorldexample.py'), Path('HelloWorldfile_without_extension'), Path('HelloWorldrelative.jpg')]
 
 
-def test_sequence():
-    """Verify that sequence transformation behaves as expected"""
-    file_sequence = range(4)
-
-    my_sequence = sequence.SequenceTransformation(file_sequence)
-    my_sequence.configure(initial=14, step=2, pad_character='a', pad_length=4)
-    resolved_result = my_sequence.resolve()
-    assert resolved_result == ['aa14', 'aa16', 'aa18', 'aa20']
+def test_suffix():
+    """Verify that suffix tranformation behaves as expected"""
+    # TODO: Determine how we want to handle things like 'example.tar.gz'
+    file_sequence = [Path('/home/test/example.py'), Path('file_without_extension'), Path('relative.jpg')]
+    transformation = suffix.SuffixTransformation(file_sequence, 'HelloWorld')
+    assert transformation.resolve() == [Path('/home/test/exampleHelloWorld.py'), Path('file_without_extensionHelloWorld'), Path('relativeHelloWorld.jpg')]
