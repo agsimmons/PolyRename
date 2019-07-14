3b0589ad2e0d9486d3f7b82dde6a0112f4b1bb1b
Andrew Simmons <agsimmons0@gmail.com>
2019-07-14 18:10:27 -0400

    Add pipeline test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 
+from PySide2.QtWidgets import QApplication
+
 from polyrename.transformation import prefix, suffix, pipeline
 from tests import TEST_SEQUENCE_01
 
 
 def test_pipeline():
     """Ensures that pipelines successfully string together transformations"""
+
+    app = QApplication([])
+
     prefix_transformation = prefix.PrefixTransformation('Hello_')
     suffix_transformation = suffix.SuffixTransformation('_World')
 
-    prefix_suffix_pipeline = pipeline.Pipeline([prefix_transformation, suffix_transformation])
+    prefix_suffix_pipeline = pipeline.Pipeline()
+    prefix_suffix_pipeline.add_transformation(prefix_transformation)
+    prefix_suffix_pipeline.add_transformation(suffix_transformation)
 
     assert prefix_suffix_pipeline.resolve(TEST_SEQUENCE_01) == [Path('/home/test/Hello_example_World.py'), Path('Hello_file_without_extension_World'), Path('Hello_relative_World.jpg'), Path('relative/path/Hello_extreme_extreme04.tar_World.gz')]
