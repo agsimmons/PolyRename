9eab93bf65662fb0f82729e2b6148c1d0a6818c6
Andrew Simmons <agsimmons0@gmail.com>
2019-07-15 16:16:43 -0400

    Refactor

--- polyrename/gui/transformation_configuration.py
+++ polyrename/gui/transformation_configuration.py
@@ -11,6 +11,9 @@ from PySide2.QtWidgets import (
 from PySide2.QtGui import QFontMetrics
 
 
+CONFIG_FIELD_HEIGHT_SCALER = 2
+
+
 class TransformationConfiguration(QGroupBox):
     def __init__(self, pipeline_editor):
         super().__init__("Transformation Config")
@@ -27,7 +30,7 @@ class TransformationConfiguration(QGroupBox):
 
     def _initialize_text(self):
         metrics = QFontMetrics(QTextEdit().font())
-        self.text_line_height = metrics.lineSpacing() * 2
+        self.text_line_height = metrics.lineSpacing() * CONFIG_FIELD_HEIGHT_SCALER
 
     def _initialize_scroll_area(self):
         scroll_area = QScrollArea()
@@ -43,19 +46,21 @@ class TransformationConfiguration(QGroupBox):
         self.layout().addWidget(scroll_area)
 
     def _initialize_add_button(self):
-        add_button = QPushButton('Add to Pipeline')
+        add_button = QPushButton("Add to Pipeline")
         add_button.clicked.connect(self.add_configured_transformation_to_pipeline)
 
         self.layout().addWidget(add_button)
+
+    def _clear_form(self):
+        for i in reversed(range(self.config_form.count())):
+            self.config_form.itemAt(i).widget().setParent(None)
 
     def swap_configuration(self, selected_transformation):
         """Swap config form with that of selected_transformation"""
 
         self.selected_transformation = selected_transformation
 
-        # Destroy existing form rows
-        for i in reversed(range(self.config_form.count())):
-            self.config_form.itemAt(i).widget().setParent(None)
+        self._clear_form()
 
         # Generate configuration for selected Transformation
         for option in selected_transformation.schema["options"]:
@@ -63,6 +68,7 @@ class TransformationConfiguration(QGroupBox):
             label = QLabel(option["name"])
             label.setToolTip(option["description"])
 
+            # TODO: Determine value of field based on default value
             field = QTextEdit("Testing")
             field.setFixedHeight(self.text_line_height)
 
@@ -80,19 +86,21 @@ class TransformationConfiguration(QGroupBox):
             configurated_field = self.config_form.itemAt(i).widget()
             form_options.append(configurated_field.toPlainText())
 
-        print('Selected options: {}'.format(form_options))
+        print("Selected options: {}".format(form_options))
 
-        option_types = [option['datatype'] for option in self.selected_transformation.schema['options']]
-        print('Option types: {}'.format(option_types))
+        option_types = []
+        for option in self.selected_transformation.schema["options"]:
+            option_types.append(option["datatype"])
+        print("Option types: {}".format(option_types))
 
         # Convert form options to correct datatype
         for i in range(len(form_options)):
             form_options[i] = option_types[i](form_options[i])
 
-        print('Casted options: {}'.format(form_options))
+        print("Casted options: {}".format(form_options))
 
         configured_transformation = self.selected_transformation(*form_options)
 
-        print('Configured Transformation: {}'.format(configured_transformation))
+        print("Configured Transformation: {}".format(configured_transformation))
 
         self.pipeline_editor.pipeline.add_transformation(configured_transformation)
