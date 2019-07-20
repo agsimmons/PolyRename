c6545a93d33e3eb63c5ab363ccbc712a9451ffee
Andrew Simmons <agsimmons0@gmail.com>
2019-07-20 13:01:32 -0400

    Improve pipeline editor layout and implement delete function

--- polyrename/gui/pipeline_editor.py
+++ polyrename/gui/pipeline_editor.py
@@ -1,10 +1,16 @@
 import shutil
 
-from PySide2.QtWidgets import QGroupBox, QVBoxLayout, QPushButton, QListView, QMessageBox
+from PySide2.QtWidgets import (
+    QGroupBox,
+    QVBoxLayout,
+    QPushButton,
+    QListView,
+    QMessageBox,
+    QHBoxLayout,
+    QWidget,
+)
 from PySide2.QtGui import QStandardItem, QStandardItemModel
-from PySide2.QtCore import QItemSelectionModel, QModelIndex
 
-from polyrename.file_sequence import FileSequence
 from polyrename.transformation.pipeline import Pipeline
 
 
@@ -23,19 +29,36 @@ class PipelineEditor(QGroupBox):
 
         self.layout().addWidget(self.pipelineView)
 
-        self.moveUpButton = QPushButton("Move Up")
-        self.moveUpButton.clicked.connect(self._move_up_listener)
-        self.moveDownButton = QPushButton("Move Down")
-        self.moveDownButton.clicked.connect(self._move_down_listener)
-        self.applyButton = QPushButton("Apply Pipeline")
-        self.applyButton.clicked.connect(self._apply_pipeline_listener)
-        # self.modifyButton = QPushButton("Modify")
-        # self.modifyButton.clicked.connect(self._modify_transformation_listener)
+        # === BUTTON CONTAINER ===
+        button_rows = QWidget()
+        self.layout().addWidget(button_rows)
+        button_rows_layout = QVBoxLayout(button_rows)
 
-        self.layout().addWidget(self.moveUpButton)
-        self.layout().addWidget(self.moveDownButton)
-        self.layout().addWidget(self.applyButton)
-        # self.layout().addWidget(self.modifyButton)
+        # === ROW 1 ===
+        row_1 = QWidget()
+        button_rows_layout.addWidget(row_1)
+        row_1_layout = QHBoxLayout(row_1)
+
+        self.moveUpButton = QPushButton("Move Up")
+        row_1_layout.addWidget(self.moveUpButton)
+        self.moveUpButton.clicked.connect(self._move_up_listener)
+
+        self.moveDownButton = QPushButton("Move Down")
+        row_1_layout.addWidget(self.moveDownButton)
+        self.moveDownButton.clicked.connect(self._move_down_listener)
+
+        self.deleteButton = QPushButton("Delete")
+        row_1_layout.addWidget(self.deleteButton)
+        self.deleteButton.clicked.connect(self._delete_listener)
+
+        # === ROW 2 ===
+        row_2 = QWidget()
+        button_rows_layout.addWidget(row_2)
+        row_2_layout = QHBoxLayout(row_2)
+
+        self.applyButton = QPushButton("Apply Pipeline")
+        row_2_layout.addWidget(self.applyButton)
+        self.applyButton.clicked.connect(self._apply_pipeline_listener)
 
         self._update_pipeline_view()
 
@@ -53,6 +76,21 @@ class PipelineEditor(QGroupBox):
         # TODO
         pass
 
+    def _move_up_listener(self):
+        to_move = self.pipelineView.selectionModel().selectedIndexes()[0].row()
+        self.pipeline.move_transformation_up(to_move)
+        self._update_pipeline_view()
+
+    def _move_down_listener(self):
+        to_move = self.pipelineView.selectionModel().selectedIndexes()[0].row()
+        self.pipeline.move_transformation_down(to_move)
+        self._update_pipeline_view()
+
+    def _delete_listener(self):
+        to_delete = self.pipelineView.selectionModel().selectedIndexes()[0].row()
+        self.pipeline.remove_transformation(to_delete)
+        self._update_pipeline_view()
+
     def _apply_pipeline_listener(self):
         file_sequence = self.file_picker.file_sequence.files
         transformed_sequence = self.pipeline.resolve(file_sequence)
@@ -62,7 +100,7 @@ class PipelineEditor(QGroupBox):
         preview_text_lines = []
         for rename in before_after:
             preview_text_lines.append(f"{rename[0].name} -> {rename[1].name}")
-        preview_text = '\n'.join(preview_text_lines)
+        preview_text = "\n".join(preview_text_lines)
 
         confirmation = QMessageBox(self)
         confirmation.setText("Are you sure you want to apply the pipeline?")
@@ -76,13 +114,3 @@ class PipelineEditor(QGroupBox):
                 shutil.move(*rename)
 
         # TODO: Either clear file sequence or reflect changes in file picker
-
-    def _move_up_listener(self):
-        to_move = self.pipelineView.selectionModel().selectedIndexes()[0].row()
-        self.pipeline.move_transformation_up(to_move)
-        self._update_pipeline_view()
-
-    def _move_down_listener(self):
-        to_move = self.pipelineView.selectionModel().selectedIndexes()[0].row()
-        self.pipeline.move_transformation_down(to_move)
-        self._update_pipeline_view()
