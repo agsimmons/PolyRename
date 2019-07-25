be191667193bee86d4944733ecb54e6109af56bd
Andrew Simmons <agsimmons0@gmail.com>
2019-07-25 16:51:19 -0400

    Python <=3.5 Fix

--- polyrename/driver/gui/pipeline_editor.py
+++ polyrename/driver/gui/pipeline_editor.py
@@ -161,5 +161,5 @@ class PipelineEditor(QGroupBox):
                 from_path = rename[0]
                 to_path = rename[1]
                 
-                shutil.move(str(from_path), str(to_path)
+                shutil.move(str(from_path), str(to_path))
             self.file_picker.clear_file_list()
