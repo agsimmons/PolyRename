55b6a57ccd0c284126c81b32e2b22019cff2c5ba
Andrew Simmons <agsimmons0@gmail.com>
2021-02-13 18:07:58 -0500

    Test with Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     author="Andrew Simmons, Ethan Smith",
     author_email="agsimmons0@gmail.com",
